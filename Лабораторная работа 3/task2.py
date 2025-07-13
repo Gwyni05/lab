0612a6f6b0e93b50b29bd7cbd80edf6d4aa9d134
Gwyni05 <syrov2126@mail.ru>
2025-07-13 20:56:55 +0300

    Лабораторная работа 3

--- Лабораторная работа 3/task2.py
+++ Лабораторная работа 3/task2.py
@@ -1,7 +1,13 @@
-# TODO Напишите функцию find_common_participants
+def find_common_participants(list_1,list_2, separator=''):
+    list_1 = list_1.split(separator)
+    list_2 = list_2.split(separator)
+    print(list_1,list_2)
+    final_list = list(set(list_1).intersection(list_2))
+    final_list.sort()
+    return final_list
 
 
 participants_first_group = "Иванов|Петров|Сидоров"
 participants_second_group = "Петров|Сидоров|Смирнов"
 
-# TODO Провеьте работу функции с разделителем отличным от запятой
+print(find_common_participants(participants_first_group,participants_second_group,"|"))
